e65c8830c08012cc81165ad300ce6b4d75ed640f
galvarado2015519 <galvarado-2015519@kinal.edu.gt>
2019-09-04 10:37:09 -0600

    Se agregaron los scanners a la clase principal

--- ProyectoColegio/src/main/java/org/colegitosa/sistema/Principal.java
+++ ProyectoColegio/src/main/java/org/colegitosa/sistema/Principal.java
@@ -2,6 +2,20 @@
 package org.colegitosa.sistema;
 
 
+import java.util.Scanner;
+import org.colegitosa.dao.AlumnoJpaController;
+import org.colegitosa.dominio.Alumno;
+
+
 public class Principal {
-    
+    private static Scanner sc = new Scanner(System.in);
+        private static Scanner salumno = new Scanner(System.in);
+        private static Alumno A;
+        private static AlumnoJpaController alumnoDao = new AlumnoJpaController();
+        private static int op, id;
+        
+        public static void main (String [] args){
+            
+                   
+        }
 }

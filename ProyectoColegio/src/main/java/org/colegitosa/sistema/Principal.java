000bb5ac2100b00a88b8c961999d7c98fd9c2203
dgarcia2018229 <dgarcia-2018229@kinal.edu.gt>
2019-09-04 11:05:48 -0600

    Creacion ce switch con sus respectivos casos

--- ProyectoColegio/src/main/java/org/colegitosa/sistema/Principal.java
+++ ProyectoColegio/src/main/java/org/colegitosa/sistema/Principal.java
@@ -2,8 +2,13 @@
 package org.colegitosa.sistema;
 
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.colegitosa.dao.AlumnoJpaController;
+import org.colegitosa.dao.exceptions.NonexistentEntityException;
 import org.colegitosa.dominio.Alumno;
 
 
@@ -14,7 +19,7 @@ public class Principal {
         private static AlumnoJpaController alumnoDao = new AlumnoJpaController();
         private static int op, id;
         
-        public static void main (String [] args){
+        public static void main (String [] args) throws Exception{
             System.out.println("--------------------------------------------");
             System.out.println("-------------------Menu---------------------");
             System.out.println("--------------------------------------------");
@@ -25,7 +30,67 @@ public class Principal {
             System.out.println("------------5. Listar Alumnos---------------");
             System.out.println("--------------------------------------------");
             System.out.println("--------Por favor Ingrese una opción--------");
-            System.out.println("--------------------------------------------");               
+            System.out.println("--------------------------------------------");   
+
+            op =sc.nextInt();
+        switch(op){
+            case 1:
+                A = obtenerAlumno();
+                System.out.println(A);
+                break;
+            
+            case 2:
+                A = getAlumno();
+            
+                
+                    if(alumnoDao.agregarAlumno(A)){
+                        System.out.println("Se agrego al nuevo alumno con exito");
+                    }else{
+                        System.out.println("No se pudo agregar al nuevo alumno");
+                    }
+                
+            
+                break;
+            
+                
+            case 3:
+                A = obtenerAlumno();
+                System.out.println("Ingresee los nombres, apellidos, telefono, codigo tecnico, codigo academico y edad nuevos");
+                A = new Alumno(id, salumno.nextLine(), salumno.nextLine(), salumno.nextLine(), salumno.nextLine(), salumno.nextLine(), sc.nextInt());
+                try {
+                    alumnoDao.editarAlumno(A);
+                } catch (Exception ex) {
+                    System.out.println("No se puede actualizar el registro");
+                }
+                
+                break;
+                
+            case 4:
+                A = obtenerAlumno();
+                System.out.println("¿Está seguro de eliminar el siguiente alumno?");
+                System.out.println(A);                      
+                try {
+                    alumnoDao.eliminarAlumno(id);
+                    System.out.println("Registro eliminado exitosamente");
+                } catch (NonexistentEntityException ex) {
+                    System.out.println("No se puede eliminar el registro");
+                }
+        
+                break;
+                
+            case 5:
+                List<Alumno> alumnos = alumnoDao.listarAlumnos();
+                for(Iterator<Alumno> iterator = alumnos.iterator(); iterator.hasNext();){
+                    Alumno next = iterator.next();
+                    System.out.println(next);
+                }
+                    
+                
+                break;
+                
+            default:
+                    System.out.println("No es una opción del menú");
+        }
         }
         
         public static Alumno obtenerAlumno(){
@@ -36,7 +101,7 @@ public class Principal {
         }
 
         private static Alumno getAlumno(){
-            System.out.println("Ingrese x atributos");
+            System.out.println("Ingrese codigo, nombres, apellidos, número telefonico, código académico, código técnico y edad del alumno");
             Alumno A =  new Alumno(sc.nextInt(), salumno.nextLine(), salumno.nextLine(), salumno.nextLine(), salumno.nextLine(), salumno.nextLine(), sc.nextInt());
             return A;
 }
